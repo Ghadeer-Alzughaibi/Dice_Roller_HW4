36963d0e7576e41f82ad830487dc46ee387a0275
Ghadeer-Alzughaibi <ghadeer.alzughaibi0@gmail.com>
2022-08-05 03:32:24 +0300

    add dice class

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,21 +4,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton : Button = findViewById(R.id.btn_roll)
-        val numberText : TextView = findViewById(R.id.txt_number)
-
+        val rollButton: Button = findViewById(R.id.btn_roll)
         rollButton.setOnClickListener {
-            val toast = Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT)
-            toast.show()
+            rollDice()
         }
+    }
+        private fun rollDice() {
+            // create object
+            val dice = Dice(6)
+            // save the result number
+            val diceRoll = dice.roll()
+
+            // reference to imageView
+            val diceImage : ImageView = findViewById(R.id.img_dice)
+
+            // check number and update screen (UI)
+            when (diceRoll) {
+                1 -> diceImage.setImageResource(R.drawable.dice_1)
+                2 -> diceImage.setImageResource(R.drawable.dice_2)
+                3 -> diceImage.setImageResource(R.drawable.dice_3)
+                4 -> diceImage.setImageResource(R.drawable.dice_4)
+                5 -> diceImage.setImageResource(R.drawable.dice_5)
+                6 -> diceImage.setImageResource(R.drawable.dice_6)
+            } // End when
+        } // End rollDice()
 
     }
-}
